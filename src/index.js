7d689184d510474b1d1908056790f0d097004bc8
Josedeodo <jose.gonzalez20957@misena.edu.co>
2022-10-11 16:40:12 -0500

    conectar a mongodb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.get('/',(req, res)=> {
-    res.send('Hello World from express!')
-})
+//conexión a la base de datos mongodb
+mongoose.connect('mongodb://localhost:27017/userscoursedb', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Conectado a MongoDB...'))
+    .catch(err => console.log('No se pudo conectar con MongoDB', err));
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
+//middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log('Api REST Ok, y ejecutándose...');
 })
