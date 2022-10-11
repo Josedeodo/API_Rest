8c2f277228b84beb7c77725c30d0dbb62caca1d0
Josedeodo <jose.gonzalez20957@misena.edu.co>
2022-10-11 18:58:41 -0500

    feature(backend) POST correct

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,10 +1,52 @@
 const express = require('express');
-const Curso = require('../models/usuario_model');
+const Usuario = require('../models/usuario_model');
 const Joi = require('@hapi/joi');
 const ruta = express.Router();
 
-ruta.get('/', (req, res)=>{
-    res.json('Respuesta a petición GET de USUARIOS funcionando correctamente...');
+//validaciones para el objeto usuario
+const schema = Joi.object({
+    nombre: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+        .pattern(/^[A-zA-záéíóú ]{3,30}$/),
+
+    password: Joi.string()
+        .pattern(/^[A-zA-Z0-9]{3,30}$/),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: {allow: ['com', 'net', 'edu', 'co']}})
 });
 
-module.exports = ruta;
+// Endpoint de tipo POST para el recurso USUARIOS
+ruta.post('/',(req,res)=>{
+    let body = req.body;
+    console.log(body)
+
+    const {error, value} = schema.validate({nombre: body.nombre, email: body.email});
+    if(!error){
+        let resultado = crearUsuario(body);
+
+        resultado.then( user => {
+            res.json({
+                valor: user
+            })
+        }).catch( err => {
+            res.status(400).json({
+                err
+            })
+        })
+    }
+});
+
+//función asíncrona para crear un objeto de tipo usuario
+async function crearUsuario(usuariox){
+    let usuario = new Usuario({
+        email       : usuariox.email,
+        nombre      : usuariox.nombre,
+        password    : usuariox.password
+    });
+return await usuario.save();
+}
+
+module.exports = ruta
