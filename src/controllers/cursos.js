11b86d09c2a2fb73838b0bac781d794585f16864
Josedeodo <jose.gonzalez20957@misena.edu.co>
2022-10-12 08:29:29 -0500

    feature(backend): Course POST succesfully

--- src/controllers/cursos.js
+++ src/controllers/cursos.js
@@ -6,4 +6,29 @@ ruta.get('/', (req, res)=>{
     res.json('Respuesta a petición GET de CURSOS funcionando correctamente...');
 });
 
+//función asíncrona para crear cursos
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo      : body.titulo,
+        descripcion : body.descripcion,
+        alumnos     : body.alumnos,
+        calificacion: body.calificacion
+    });
+    return await curso.save();
+}
+
+//End point de tipo POST para el recurso CURSOS
+ruta.post('/',(req, res)=>{
+    let resultado = crearCurso(req.body);
+    resultado.then(curso =>{
+        res.json({
+            curso
+        })
+    }).catch(err =>{
+        res.status(400).json({
+            err
+        })
+    })
+});
+
 module.exports = ruta;
