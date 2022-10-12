816db533c29107870f4f3a0554ce6804c4290705
Josedeodo <jose.gonzalez20957@misena.edu.co>
2022-10-12 07:50:52 -0500

    feature(backend): function put succesfully

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Usuario = require('../models/usuario_model');
 const Joi = require('@hapi/joi');
+const usuario_model = require('../models/usuario_model');
 const ruta = express.Router();
 
 //validaciones para el objeto usuario
@@ -18,10 +19,19 @@ const schema = Joi.object({
         .email({ minDomainSegments: 2, tlds: {allow: ['com', 'net', 'edu', 'co']}})
 });
 
+//función asíncrona para crear un objeto de tipo usuario
+async function crearUsuario(usuariox){
+    let usuario = new Usuario({
+        email       : usuariox.email,
+        nombre      : usuariox.nombre,
+        password    : usuariox.password
+    });
+return await usuario.save();
+}
+
 // Endpoint de tipo POST para el recurso USUARIOS
 ruta.post('/',(req,res)=>{
     let body = req.body;
-    console.log(body)
 
     const {error, value} = schema.validate({nombre: body.nombre, email: body.email});
     if(!error){
@@ -39,14 +49,32 @@ ruta.post('/',(req,res)=>{
     }
 });
 
-//función asíncrona para crear un objeto de tipo usuario
-async function crearUsuario(usuariox){
-    let usuario = new Usuario({
-        email       : usuariox.email,
-        nombre      : usuariox.nombre,
-        password    : usuariox.password
-    });
-return await usuario.save();
-}
+//función asíncrona para actualizar usuario
+async function actualizarUsuario(email, body){
+    let usuario = await Usuario.findOneAndUpdate({"email":email},{
+        $set: {
+            nombre : body.nombre,
+            password: body.password
+        }
+    }, {new: true});
+    return usuario;
+} 
+
+// End point de tipo PUT para actualizar los datos del usuario
+ruta.put('/:email', (req, res) => {
+    const {error, value} = schema.validate({nombre: req.body.nombre});
+    if(!error){
+        let resultado = actualizarUsuario(req.params.email, req.body);
+        resultado.then(valor => {
+            res.json({
+                valor
+            })
+        });
+    }else{
+        res.status(400).json({
+            error
+        })
+    }
+});
 
 module.exports = ruta
